66b2b4b39c6ccb1699f3c858ebd2498e05dd3812
lionsinex <1493695768@qq.com>
2020-01-24 16:08:53 +0800

    '移动目录'

--- weixin/weixin/settings.py
+++ weixin/weixin/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # django应用
+    "apps"
 ]
 
 MIDDLEWARE = [
@@ -75,7 +77,7 @@ WSGI_APPLICATION = 'weixin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
